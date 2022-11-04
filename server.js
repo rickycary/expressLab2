c794c684eb83ee970cf04c96515249f2aa19d745
Richard <ricky.cary1@gmail.com>
2022-11-04 11:53:10 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/greeting/:name', (request, response) => {
     response.send(`Hello ${request.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+    console.log(request.params)
+    response.send(`Your total is $${request.params.total} and you tipped ${request.params.tipPercentage}%`)
+})
+
 app.listen(port, () => {
     console.log('Express is listening in port: ', port)
 })
